861a650e11d9e2e15bedae2197ce5c70dcfab499
ryozm <ryozming@gmail.com>
2024-01-25 01:44:55 +0800

    feat: Add dropdown menu to header component

--- app/home/components/Header.tsx
+++ app/home/components/Header.tsx
@@ -1,13 +1,32 @@
 'use client'
 import { useState, SVGProps } from 'react'
 import { Button } from '@/components/ui/button'
-import { Moon, Sun } from 'lucide-react'
+import {
+  Book,
+  Briefcase,
+  ChevronsDownUp,
+  Mail,
+  Menu,
+  Moon,
+  Sun,
+  User
+} from 'lucide-react'
 import { useTheme } from 'next-themes'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
+import { cn } from '@/lib/utils'
 
 const Header = () => {
   const { theme, setTheme } = useTheme()
 
   const [logoHover, setLogoHover] = useState(false)
+
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <header className="fixed z-20 shadow-lg top-4 left-4 right-4 px-4 lg:px-6 h-16 bg-gray-200 dark:bg-gray-700 text-gray-800 justify-between flex flex-row items-center rounded-[40px]">
       <div
@@ -24,10 +43,60 @@ const Header = () => {
         </span>
       </div>
       <div className="flex flex-row items-center gap-2 font-bold">
-        <Button variant="link">about</Button>
-        <Button variant="link">work</Button>
-        <Button variant="link">post</Button>
-        <Button variant="link">contact</Button>
+        <Button className="hidden md:flex" variant="link">
+          <User className="mr-2 h-4 w-4" />
+          About
+        </Button>
+        <Button className="hidden md:flex" variant="link">
+          <Briefcase className="mr-2 h-4 w-4" />
+          Works
+        </Button>
+        <Button className="hidden md:flex" variant="link">
+          <Book className="mr-2 h-4 w-4" />
+          Posts
+        </Button>
+        <Button className="hidden md:flex" variant="link">
+          <Mail className="mr-2 h-4 w-4" />
+          Contact
+        </Button>
+        <DropdownMenu open={menuOpen} onOpenChange={open => setMenuOpen(open)}>
+          <DropdownMenuTrigger asChild>
+            <Button className="flex md:hidden" variant="link">
+              <Menu
+                className={cn(
+                  !menuOpen ? 'scale-100 rotate-0' : 'scale-0 rotate-180',
+                  'h-6 w-6 transition-all duration-500'
+                )}
+              />
+              <ChevronsDownUp
+                className={cn(
+                  menuOpen ? 'scale-100 rotate-0' : 'scale-0 rotate-180',
+                  'absolute h-6 w-6 transition-all duration-500'
+                )}
+              />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuGroup>
+              <DropdownMenuItem>
+                <User className="mr-2 h-4 w-4" />
+                <span>About</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Briefcase className="mr-2 h-4 w-4" />
+                <span>Works</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Book className="mr-2 h-4 w-4" />
+                <span>Posts</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Mail className="mr-2 h-4 w-4" />
+                <span>Contact</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
         <Button
           className="rounded-full"
           onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
