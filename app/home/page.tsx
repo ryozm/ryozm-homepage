c5aaca937cc675880f8ed75c2dbdc2c1c2284628
ryozm <ryozming@gmail.com>
2024-01-25 01:45:18 +0800

    feat:  Update styling in home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,3 +1,4 @@
+import { Book, Briefcase, Mail, User } from 'lucide-react'
 import DynamicLogo from './components/DynamicLogo'
 import Header from './components/Header'
 
@@ -5,8 +6,8 @@ const Home = () => {
   return (
     <div className=" snap-y snap-mandatory overflow-y-auto flex flex-col h-screen w-full">
       <Header />
-      <div className="snap-always relative snap-center min-h-screen pt-[100px] bg-gradient-to-b from-red-500 to-blue-500 dark:from-slate-800 dark:to-black">
-        <div className="text-center px-4 lg:px-10">
+      <div className="snap-always border-t-2 relative snap-center min-h-screen pt-[100px] bg-slate-400 dark:bg-slate-900">
+        <div className="text-center">
           <h3 className="text-3xl italic">Hello,I'm Ryozm</h3>
           <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
             I'm a front-end developer ❤️.
@@ -16,68 +17,28 @@ const Home = () => {
           <DynamicLogo />
         </div>
       </div>
-      <div className="snap-always snap-center min-h-screen pt-[100px] bg-gradient-to-b from-red-500 to-blue-500 dark:from-slate-800 dark:to-black">
-        <div className="text-center px-4 lg:px-10">
-          <h3 className="text-3xl italic">Hello,I'm Ryozm</h3>
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-            I'm a front-end developer ❤️.
-          </h1>
+      <div className="snap-always border-t-2 snap-center min-h-screen pt-[100px] bg-slate-400 dark:bg-slate-900">
+        <div className="text-3xl italic flex justify-center items-center">
+          <User className="mr-2 h-8 w-8" />
+          About
         </div>
       </div>
-      <div className="snap-always snap-center min-h-screen pt-[100px] bg-gradient-to-b from-red-500 to-blue-500 dark:from-slate-800 dark:to-black">
-        <div className="text-center px-4 lg:px-10">
-          <h3 className="text-3xl italic">Hello,I'm Ryozm</h3>
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-            I'm a front-end developer ❤️.
-          </h1>
+      <div className="snap-always border-t-2 snap-center min-h-screen pt-[100px] bg-slate-400 dark:bg-slate-900">
+        <div className="text-3xl italic flex justify-center items-center">
+          <Briefcase className="mr-2 h-8 w-8" />
+          Works
         </div>
       </div>
-      <div className="snap-always snap-center min-h-screen pt-[100px] bg-gradient-to-b from-red-500 to-blue-500 dark:from-slate-800 dark:to-black">
-        <div className="text-center px-4 lg:px-10">
-          <h3 className="text-3xl italic">Hello,I'm Ryozm</h3>
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-            I'm a front-end developer ❤️.
-          </h1>
+      <div className="snap-always border-t-2 snap-center min-h-screen pt-[100px] bg-slate-400 dark:bg-slate-900">
+        <div className="text-3xl italic flex justify-center items-center">
+          <Book className="mr-2 h-8 w-8" />
+          Posts
         </div>
       </div>
-      <div className="snap-always snap-center min-h-screen pt-[100px] bg-gradient-to-b from-red-500 to-blue-500 dark:from-slate-800 dark:to-black">
-        <div className="text-center px-4 lg:px-10">
-          <h3 className="text-3xl italic">Hello,I'm Ryozm</h3>
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-            I'm a front-end developer ❤️.
-          </h1>
-        </div>
-      </div>
-      <div className="snap-always snap-center min-h-screen pt-[100px] bg-gradient-to-b from-red-500 to-blue-500 dark:from-slate-800 dark:to-black">
-        <div className="text-center px-4 lg:px-10">
-          <h3 className="text-3xl italic">Hello,I'm Ryozm</h3>
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-            I'm a front-end developer ❤️.
-          </h1>
-        </div>
-      </div>
-      <div className="snap-always snap-center min-h-screen pt-[100px] bg-gradient-to-b from-red-500 to-blue-500 dark:from-slate-800 dark:to-black">
-        <div className="text-center px-4 lg:px-10">
-          <h3 className="text-3xl italic">Hello,I'm Ryozm</h3>
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-            I'm a front-end developer ❤️.
-          </h1>
-        </div>
-      </div>
-      <div className="snap-always snap-center min-h-screen pt-[100px] bg-gradient-to-b from-red-500 to-blue-500 dark:from-slate-800 dark:to-black">
-        <div className="text-center px-4 lg:px-10">
-          <h3 className="text-3xl italic">Hello,I'm Ryozm</h3>
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-            I'm a front-end developer ❤️.
-          </h1>
-        </div>
-      </div>
-      <div className="snap-always snap-center min-h-screen pt-[100px] bg-gradient-to-b from-red-500 to-blue-500 dark:from-slate-800 dark:to-black">
-        <div className="text-center px-4 lg:px-10">
-          <h3 className="text-3xl italic">Hello,I'm Ryozm</h3>
-          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-            I'm a front-end developer ❤️.
-          </h1>
+      <div className="snap-always border-t-2 snap-center min-h-screen pt-[100px] bg-slate-400 dark:bg-slate-900">
+        <div className="text-3xl italic flex justify-center items-center">
+          <Mail className="mr-2 h-8 w-8" />
+          Contact
         </div>
       </div>
     </div>
